a109913833a21b0eb5167f4fbec0cdd2d36c7c30
laivey <lannesims@gmail.com>
2017-04-11 21:42:54 -0400

    Day 5 Homework
    Homework using Javascript functions

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+
+// 1
+
+function addNumbers(numberA, numberB) {
+  return numberA + numberB;
+
+}
+
+var twoPlusTwo = addNumbers(2, 2);
+
+console.log(twoPlusTwo);
+
+
+
+// 2
+function yell(holler) {
+return holler.toUpperCase();
+
+}
+
+console.log(yell("This is nuts"));
+
+
+// 2 part 2
+function yell10(holler) {
+return holler.toUpperCase();
+
+}
+
+var i = 0;
+while (i < 10){
+  console.log(yell10("This is nuts")+i);
+  i++;
+}
+console.log(yell10(yell("This is nuts")));
+
+// 3
+
+
+console.log([540, 206, 0629].length);
+
+
+var fruits = ["strawberry", "banana", "cococnut", "apples"];
+console.log(fruits.length);
+
+
+function longest() {
+  return 
+  var cereal = ["RaisinBran", "FrostedFlakes", "Kix", "Cheerios"];
+}
+
+
+
+    console.log(cereal.length);
+
+// 4
